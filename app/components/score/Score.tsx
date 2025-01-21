8b52470b7837bdc14da2001ea80ba0107460ed4e
Pitra <justin.pitra@gmail.com>
2025-01-21 13:21:45 +0100

    score component

---
+++ app/components/score/Score.tsx
@@ -0,0 +1,11 @@
+const Score = () => {
+    return ( 
+        <div>
+            <h1>
+                This is the score !! 🏆
+            </h1>
+        </div>
+     );
+}
+ 
+export default Score;
