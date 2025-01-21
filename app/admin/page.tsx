104e9a5a3ade369b796722833d189edc6de34143
Pitra <justin.pitra@gmail.com>
2025-01-21 12:55:32 +0100

    test socket

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,47 @@
-export default function Admin() {
-  return <h1>Admin</h1>;
-}
+'use client'
+
+import { useEffect, useState } from "react";
+import socket from "../api/socket";
+
+export default function Admin()  {
+  const [connected, setConnected] = useState<boolean>(false);
+  const [infoMessage, setInfoMessage] = useState<string>("");
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connecté au WebSocket");
+      setConnected(true);
+    });
+
+    socket.on("info", (data: { message: string }) => {
+      console.log("Message reçu du serveur :", data);
+      setInfoMessage(data.message);
+    });
+
+    socket.on("connect_error", (err) => {
+      console.error("Erreur de connexion :", err);
+      setConnected(false);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Déconnecté du WebSocket");
+      setConnected(false);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("info");
+      socket.off("connect_error");
+      socket.off("disconnect");
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Connexion au WebSocket </h1>
+      <p>Statut : {connected ? "Connecté" : "Non connecté"}</p>
+      <p>Message reçu : {infoMessage || "Aucun message pour l'instant"}</p>
+    </div>
+  );
+};
+
