f3d7d926d753ed440a9e1800f8041ae327769c1d
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2025-01-27 11:42:12 +0100

    loading screen for tv

---
+++ app/tv/loading/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useWebSocket } from '@/app/context/WebSocketUsage';
+import LoadingTvScreen from '@/app/components/loading-tv/LoadingTv';
+
+const LoadingPage = () => {
+  const router = useRouter();
+  const {
+    isConnected,
+    loadingState,
+    setLoadingState,
+    registerEventHandler,
+    unregisterEventHandler,
+  } = useWebSocket();
+
+  useEffect(() => {
+    const handleStartGame = () => {
+      console.log('Start game event received');
+      setLoadingState('starting');
+      const timeoutId = setTimeout(() => {
+        router.push(`/tv/score`);
+      }, 1000);
+
+      return () => clearTimeout(timeoutId); // Nettoie le timeout
+    };
+
+    registerEventHandler('startGame', handleStartGame);
+
+    return () => {
+      unregisterEventHandler('startGame', handleStartGame);
+    };
+  }, [router, setLoadingState, registerEventHandler, unregisterEventHandler]);
+
+  return <LoadingTvScreen state={loadingState} />;
+};
+
+export default LoadingPage;
